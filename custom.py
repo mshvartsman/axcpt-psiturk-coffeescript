e64344b94a32ee1ebb722396c52f4e2dced36e23
Mike Shvartsman <ms44@princeton.edu>
2015-06-23 16:07:47 -0400

    Debug for bonus assignment

--- custom.py
+++ custom.py
@@ -86,7 +86,7 @@ def compute_bonus():
 
         for record in user_data['data']: # for line in data file
             trial = record['trialdata']
-            if trial['blockID'] != "Practice" && trial['blockID'] != "Test":
+            if trial['blockID'] != "Practice" and trial['blockID'] != "Test":
                 bonus += trial['bonus']
             # if trial['phase']=='TEST':
                 # if trial['hit']==True:
